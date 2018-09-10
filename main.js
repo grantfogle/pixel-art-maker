69c61c36409da8fb7379e2a17a6e37468f76078a
grantfogle <grantjfogle@gmail.com>
2018-09-10 15:49:25 -0600

    made grid into table rather than divs

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //select .pixel elements, creates our canvas
     var canvas = document.getElementsByClassName("canvas");
     var pixel = document.getElementsByClassName("pixel");
-    var clientWidth = canvas[0].clientWidth;
-    var clientHeight = canvas[0].clientHeight;
-    var clientArea = clientWidth * clientHeight;
-    var divArea = 0;
+    var canvasWidth = canvas[0].clientWidth;
+    var canvasHeight = canvas[0].clientHeight;
+    var canvasArea = canvasWidth * canvasHeight;
 
-    while (clientArea > divArea) {
-        var div = document.createElement("DIV");
-        div.classList.add("pixel");
-        canvas[0].append(div);
-        divArea += 175;
+    var table = document.getElementsByClassName("canvas-table");
+    let tableHeight = table[0].clientHeight;
+    let tableWidth = table[0].clientWidth;
+    let tableArea = tableHeight * tableWidth;
+
+    for (let i = 0; i < table.length; i++) {
+        for (let k = 0; k < canvasHeight; k += 40) {
+            var tr = document.createElement("TR");
+            for (let j = 0; j < canvasWidth; j += 30) {
+                var td = document.createElement("TD");
+                td.classList.add("pixel");
+                tr.appendChild(td);
+            }
+            table[i].appendChild(tr);
+        }
     }
 
-
-    //when clicking a paint-brush box, change class to certain color
-    var brushColor = "red";
-    // var currentColor = document.getElementByID("current-color-box").style.backgroundColor;
-
+    var brushColor = "firebrick";
     var brush = document.getElementsByClassName("brush");
-    console.log(canvas);
-    console.log(pixel);
-
-
 
     for (let i = 0; i < brush.length; i++) {
         brush[i].addEventListener("click", function (event) {
@@ -32,36 +32,19 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    //change color when clicked
-    //add brush functionality function -- This works!
-    // for (let i = 0; i < pixel.length; i++) {
-    //     pixel[i].addEventListener("mousedown", function () {
-    //         event.target.style.backgroundColor = brushColor;
-    //     });
-    // }
 
-    //change functionality of brush
-    //mousedown
-    var brushFunction = "click";
     var fill = document.getElementById("flood");
 
-    //make it so all of the pixels are flooded with that color
     fill.addEventListener("click", function () {
         for (let i = 0; i < pixel.length; i++) {
             pixel[i].style.backgroundColor = brushColor;
         }
     });
 
-    //while mousedown is clicked = true
-    //change colors on mouseenter function 
-    //mouse up is clicked  false
 
     var isClicked = false;
 
-    //inside a pixel for loop
     for (let i = 0; i < pixel.length; i++) {
-        // if (mousedown == true) {
-        // pixel[i].addEventListener(
         document.addEventListener("mousedown", function () {
             isClicked = true;
             pixel[i].addEventListener("mouseover", function () {
@@ -72,7 +55,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         isClicked = false;
 
-        //on mouseup, isclicked is false
         document.addEventListener("mouseup", function () {
             isClicked = false;
         })
@@ -83,19 +65,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     }
-    // if 
-
-    // mouseup, mousenter, change
-    //create a grid layout
-
-    // var row = 
-
-    // div.classList.add("pixel")
-
-    // while ()
-
-    // var h1Span = document.querySelector("h1 span");
-    // var countText = h1Span.textContent.slice(1, -1)
-    // var countNum = parseInt(countText)
 
 })
